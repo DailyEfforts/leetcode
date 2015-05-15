6def24bb309b1008730d121be5a45aba1360cdd4
vhow <vhow@163.com>
2015-05-15 21:31:24 +0800

    Add a solution to `Implement strStr()` problem on leetcode.

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ ID|Problem|Solution
 118|[Pascal's Triangle](https://leetcode.com/problems/pascals-triangle/)|[Java](./algorithms/src/pascals_triangle/Solution.java)
 112|[Path Sum](https://leetcode.com/problems/path-sum/)|[Java](./algorithms/src/path_sum/Solution.java)
 83|[Remove Duplicates from Sorted List](https://leetcode.com/problems/remove-duplicates-from-sorted-list/)|[Java](./algorithms/src/remove_duplicates/Solution.java)
+28|[Implement strStr()](https://leetcode.com/problems/implement-strstr/)|[Java](./algorithms/src/implement_strstr/Solution.java)
 7|[Reverse Integer](https://leetcode.com/problems/reverse-integer/  https://leetcode.com/problems/reverse-integer/)|[Java](./algorithms/src/reverse_integer/Solution.java)
 2|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)|[Java](./algorithms/src/add_two_numbers/Solution.java)
 1|[Two Sum](https://leetcode.com/problems/two-sum/)|[Java](./algorithms/src/two_sum/Solution.java)
---
+++ algorithms/src/implement_strstr/Solution.java
@@ -0,0 +1,40 @@
+package implement_strstr;
+
+/**
+ * Implement strStr().
+ * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ */
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        final int hayStackLen = haystack.length();
+        final int needleLen = needle.length();
+        if (hayStackLen < needleLen) {
+            return -1;
+        }
+        if (hayStackLen == 0 && needleLen == 0) {
+            return 0;
+        }
+        for (int i = 0; i < hayStackLen; i++) {
+            if (hayStackLen - i < needleLen) {
+                break;
+            }
+            int j = 0;
+            int k = i;
+            while (j < needleLen && k < hayStackLen && haystack.charAt(k) == needle.charAt(j)) {
+                k++;
+                j++;
+            }
+            if (j == needleLen) {
+                return k - needleLen;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        System.out.println(solution.strStr("", ""));
+        System.out.println(solution.strStr("mississippi", "issip"));
+    }
+}
+
